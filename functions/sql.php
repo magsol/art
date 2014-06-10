0b11e8d32d8493253aafb86aca44b539d5157da8
Shannon Quinn <magsol@gmail.com>
2014-06-10 17:55:19 -0400

    Added a delete method.

--- functions/sql.php
+++ functions/sql.php
@@ -2,11 +2,13 @@
 
 function update_oauth($conn, $handle, $token, $secret) {
     $sql = 'SELECT * FROM users WHERE handle = ?';
-    $result = mysqli_query($conn, "SELECT * FROM users WHERE handle = '" .
-        $handle . "'");
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 's', $handle);
+    $results = mysqli_stmt_execute($stmt);
     $rows = mysqli_num_rows($result);
     if ($rows > 0) {
         // User exists; do an update.
+        mysqli_stmt_close($stmt);
         $sql = "UPDATE users SET access_token = ?, access_secret = ?, " . 
                 "WHERE handle = ?";
         $stmt = mysqli_prepare($conn, $sql);
@@ -16,6 +18,18 @@ function update_oauth($conn, $handle, $token, $secret) {
             return false;
         }
         mysqli_stmt_close($stmt);
+    } else {
+        // Doesn't exist; do an insert.
+        mysqli_stmt_close($stmt);
+        $sql = 'INSERT INTO users (handle, access_token, access_secret) VALUES ' .
+                '(?, ?, ?)';
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, 'sss', $handle, $token, $secret);
+        $results = mysqli_stmt_execute($stmt);
+        if ($results === false) {
+            return false;
+        }
+        mysqli_Stmt_close($stmt);
     }
     return true;
 }
@@ -27,6 +41,18 @@ function update_gc($conn, $handle, $user, $pass, $hour, $min) {
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, 'ssiis', $user, $pass, $hour, $min,
         $handle);
+    $results = mysqli_stmt_execute($stmt);
+    if ($results === false) {
+        return false;
+    }
+    mysqli_stmt_close($stmt);
+    return true;
+}
+
+function delete_user($conn, $handle) {
+    $sql = 'DELETE FROM users WHERE handle = ?';
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 's', $handle);
     $results = mysqli_stmt_execute($stmt);
     if ($results === false) {
         return false;
