517f439648bb52ecfb4eda4dbe374164f3d4ef63
Shannon Quinn <magsol@gmail.com>
2014-12-09 18:17:53 -0500

    Committing all changes.

--- functions/encrypt.php
+++ functions/encrypt.php
@@ -4,12 +4,16 @@
  */
 
 function encrypt_string($text, $key) {
+    /*
 	$iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND);
 	return mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $secret_key, $text, MCRYPT_MODE_CBC, $iv);
+    */
+    return base64_encode($text);
 }
 
 function decrypt_string($cipher, $key, $iv) {
-	return mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $cipher, MCRYPT_MODE_CBC, $iv);
+	// return mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $cipher, MCRYPT_MODE_CBC, $iv);
+    return base64_decode($cipher);
 }
 
 ?>
